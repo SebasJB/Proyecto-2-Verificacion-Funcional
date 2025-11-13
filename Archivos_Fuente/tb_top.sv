478d9770bf888dd83d63c9f741f394aff466d2e6
TayronRS <ruiztayron0@gmail.com>
2025-11-13 14:07:06 -0600

    modificación

--- Archivos_Fuente/tb_top.sv
+++ Archivos_Fuente/tb_top.sv
@@ -95,20 +95,21 @@ module tb_top;
   // ---------------- Pasar VIFs a tus agentes reales (agt0..agt15, d0/m0) ----
   // Agent se llama "agt%0d" y dentro tiene "d0" (driver) y "m0" (monitor).
 // set por índice constante (genvar)
-  initial begin
-    for (int k = 0; k < N_TERMS; k++) begin
-      uvm_config_db#(virtual router_if #(.PCK_SZ(PCK_SZ)))::set(
-        null, $sformatf("uvm_test_top.e.agt%0d.drv", k), "vif", term_if[k]
-      );
-      uvm_config_db#(virtual router_if #(.PCK_SZ(PCK_SZ)))::set(
-        null, $sformatf("uvm_test_top.e.agt%0d.mon", k), "vif", term_if[k]
-      );
-    end
-    // Llama explícitamente a tu test por nombre para evitar depender de +UVM_TESTNAME
-    run_test("base_test");
-  end
-  
+    generate
+      for (genvar g = 0; g < N_TERMS; g++) begin : CFG
+        initial begin
+          uvm_config_db#(virtual router_if#(PCK_SZ))::set(
+            null, $sformatf("uvm_test_top.env.agt%0d.drv", g), "vif", term_if[g]
+          );
+          uvm_config_db#(virtual router_if#(PCK_SZ))::set(
+            null, $sformatf("uvm_test_top.env.agt%0d.mon", g), "vif", term_if[g]
+          );
+        end
+      end
+    endgenerate
 
+  
+  initial run_test("base_test");
 
   initial begin 
     $fsdbDumpfile("waves.fsdb"); 
