d636c72e7783701f4a5bb6b3e1216c4382e60ffe
Sebastián Barrantes <sbarrantes0909@gmail.com>
2025-11-16 00:06:05 -0600

    Corrección

--- Archivos_Fuente/monitor.sv
+++ Archivos_Fuente/monitor.sv
@@ -59,7 +59,7 @@ class monitor extends uvm_monitor;
         item.time_stamp = $time;
         `uvm_info(get_type_name(),
                   $sformatf("[MON] data_out=0x%0h @%0t", item.data, item.time_stamp),
-                  UVM_LOW)
+                  UVM_MEDIUM)
         mon_analysis_port.write(item);
 
         // Bajar pop en el próximo ciclo
