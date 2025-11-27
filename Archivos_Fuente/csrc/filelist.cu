7d97977a0527cb5a62eba6db38bdcee2823b76df
SebasJB <sbarrantes0909@gmail.com>
2025-11-26 23:50:35 -0600

    Corrida

--- Archivos_Fuente/csrc/filelist.cu
+++ Archivos_Fuente/csrc/filelist.cu
@@ -5,16 +5,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _463323_archive_1.so
-_463323_archive_1.so : archive.82/_463323_archive_1.a
+ARCHIVE_OBJS += _493273_archive_1.so
+_493273_archive_1.so : archive.87/_493273_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_463323_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_493273_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../salida.daidir//_463323_archive_1.so $@
+	@ln -sf .//../salida.daidir//_493273_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.82/_prev_archive_1.a
+_prev_archive_1.so : archive.87/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- Archivos_Fuente/verdiLog/verdi.cmd
+++ Archivos_Fuente/verdiLog/verdi.cmd
@@ -1,31 +1,34 @@
-simSetSimulator "-vcssv" -exec "./salida" -args "+UVM_TIMEOUT=38000" -uvmDebug on \
+simSetSimulator "-vcssv" -exec "./salida" -args "+UVM_TIMEOUT=30000" -uvmDebug on \
            -simDelim
 debImport "-i" "-simflow" "-dbdir" "./salida.daidir"
 srcTBInvokeSim
-srcHBSelect "tb_top.term_if\[3\]" -win $_nTrace1
-srcSetScope "tb_top.term_if\[3\]" -delim "." -win $_nTrace1
-srcHBSelect "tb_top.term_if\[3\]" -win $_nTrace1
-srcSignalViewSelect "tb_top.term_if\[3\].clk"
-srcSignalViewSelect "tb_top.term_if\[3\].clk" \
-           "tb_top.term_if\[3\].data_out\[39:0\]"
-srcSignalViewSelect "tb_top.term_if\[3\].clk" \
-           "tb_top.term_if\[3\].data_out\[39:0\]" "tb_top.term_if\[3\].pop"
-srcSignalViewSelect "tb_top.term_if\[3\].clk" \
-           "tb_top.term_if\[3\].data_out\[39:0\]" "tb_top.term_if\[3\].pop" \
-           "tb_top.term_if\[3\].pndng"
+srcHBSelect "tb_top.term_if\[0\]" -win $_nTrace1
+srcSetScope "tb_top.term_if\[0\]" -delim "." -win $_nTrace1
+srcHBSelect "tb_top.term_if\[0\]" -win $_nTrace1
+srcSignalViewSelect "tb_top.term_if\[0\].data_in\[39:0\]"
+srcSignalViewSelect "tb_top.term_if\[0\].clk"
+srcSignalViewSelect "tb_top.term_if\[0\].data_in\[39:0\]"
+srcSignalViewSelect "tb_top.term_if\[0\].data_in\[39:0\]" \
+           "tb_top.term_if\[0\].pndng_in" "tb_top.term_if\[0\].popin" \
+           "tb_top.term_if\[0\].data_out\[39:0\]" "tb_top.term_if\[0\].pop" \
+           "tb_top.term_if\[0\].pndng"
+srcSignalViewSelect "tb_top.term_if\[0\].data_in\[39:0\]" \
+           "tb_top.term_if\[0\].pndng_in" "tb_top.term_if\[0\].popin" \
+           "tb_top.term_if\[0\].data_out\[39:0\]" "tb_top.term_if\[0\].pop" \
+           "tb_top.term_if\[0\].pndng" "tb_top.term_if\[0\].clk"
 wvCreateWindow
 srcSignalViewAddSelectedToWave -win $_nTrace1 -clipboard
 wvDrop -win $_nWave3
 srcTBRunSim
-wvZoomIn -win $_nWave3
-wvZoomIn -win $_nWave3
-wvZoomOut -win $_nWave3
-wvZoomOut -win $_nWave3
-wvSetCursor -win $_nWave3 36002.754545 -snap {("G1" 1)}
-wvZoomIn -win $_nWave3
-wvZoomIn -win $_nWave3
-wvSetCursor -win $_nWave3 36048.369838 -snap {("G1" 3)}
-wvSetCursor -win $_nWave3 36049.427797 -snap {("G1" 3)}
-wvSetCursor -win $_nWave3 36050.133103
-wvSetCursor -win $_nWave3 36044.490657 -snap {("G1" 1)}
+srcHBSelect "tb_top.CFG\[13\]" -win $_nTrace1
+srcHBSelect "tb_top.term_if\[0\]" -win $_nTrace1
+srcSetScope "tb_top.term_if\[0\]" -delim "." -win $_nTrace1
+srcHBSelect "tb_top.term_if\[0\]" -win $_nTrace1
+srcSignalViewSelect "tb_top.term_if\[0\].reset"
+wvSetPosition -win $_nWave3 {("G2" 0)}
+wvSetPosition -win $_nWave3 {("G1" 7)}
+wvAddSignal -win $_nWave3 "/tb_top/term_if\[0\]/reset"
+wvSetPosition -win $_nWave3 {("G1" 7)}
+wvSetPosition -win $_nWave3 {("G1" 8)}
+wvSetCursor -win $_nWave3 97.751711 -snap {("G2" 0)}
 debExit
